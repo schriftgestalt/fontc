1562460efa81c6c34aba08406b5425604e8f7b7f
Martin Winter <developer@martinwinter.com>
2025-06-27 08:01:52 +0100

    Add case for Glyphs app targets to target description function.

--- fontc_crater/src/ci/html.rs
+++ fontc_crater/src/ci/html.rs
@@ -509,6 +509,7 @@ fn make_target_description(target: &Target) -> Markup {
             }
             _ => "gftools".to_string(),
         },
+        BuildType::GlyphsApp => "glyphs_app".to_string(),
     };
     html! {
         (source)
