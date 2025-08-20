c198aec3c162426f824869a000ff5cee4abc40d8
Martin Winter <developer@martinwinter.com>
2025-08-20 08:50:26 +0100

    Remove unused function.

--- fontc_crater/src/run_conf.rs
+++ fontc_crater/src/run_conf.rs
@@ -123,10 +123,6 @@ impl RunConfiguration {
         }
     }
 
-    pub fn has_glyphs_app(&self) -> bool {
-        self.tool_pairs.iter().any(|pair| pair.has_glyphs_app())
-    }
-
     fn tool_pair_for_category(&self) -> &ToolPair {
         &self.tool_pairs[0]
     }
